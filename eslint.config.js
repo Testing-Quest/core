38aeb10ea2c0f54351c5a7f7b88b8740cd9767ff
pablo-snz <pablo.snz.brm@gmail.com>
2024-09-15 17:25:43 +0200

    [fix] several errors

--- eslint.config.js
+++ eslint.config.js
@@ -1,36 +1,49 @@
-import js from '@eslint/js';
-import typescriptPlugin from '@typescript-eslint/eslint-plugin';
-import typescriptParser from '@typescript-eslint/parser';
-import reactHooks from 'eslint-plugin-react-hooks';
-import reactRefresh from 'eslint-plugin-react-refresh';
-import prettierPlugin from 'eslint-plugin-prettier';
-import prettierConfig from 'eslint-config-prettier';
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import tseslint from 'typescript-eslint'
+import path from 'path';
+import { fileURLToPath } from 'url';
 
-export default [
-  js.configs.recommended,
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default tseslint.config(
+  { ignores: ['dist', 'node_modules'] },
   {
-    files: ['**/*.ts', '**/*.tsx'],
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.all,
+      //...tseslint.configs.stylisticTypeChecked,
+      //...tseslint.configs.recommended,
+    ],
+    files: ['src/**/*.{ts,tsx}', 'test/**/*.{ts,tsx}'],
     languageOptions: {
-      parser: typescriptParser,
       ecmaVersion: 2020,
-      sourceType: 'module',
+      globals: globals.browser,
+      parserOptions: {
+        project: './tsconfig.json',
+        tsconfigRootDir: __dirname,
+      },
     },
     plugins: {
-      '@typescript-eslint': typescriptPlugin,
-      'react-refresh': reactRefresh,
-      'prettier': prettierPlugin,
       'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
     },
     rules: {
+      ...reactHooks.configs.recommended.rules,
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },
       ],
-      'prettier/prettier': 'error',
+      '@typescript-eslint/no-unused-vars': 'warn',
+      '@typescript-eslint/no-magic-numbers': 'off',
+      '@typescript-eslint/consistent-type-definitions': [
+        'error',
+        'interface',
+      ],
+      '@typescript-eslint/prefer-readonly-parameter-types': 'off',
     },
   },
-  prettierConfig,
-  {
-    ignores: ['dist', '.eslintrc.cjs'],
-  },
-];
+)
